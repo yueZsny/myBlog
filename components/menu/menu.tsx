0e1a99a78a8f57b775d1fd8dea2addd4bb595c22
zy <2799962654@qq.com>
2025-10-30 09:14:13 +0800

    fix:修复了本地的问题

---
+++ components/menu/menu.tsx
@@ -0,0 +1,8 @@
+import './menu.css'
+export default function menu() {
+  return(
+    <div className="menu">
+      <div>1</div>
+    </div>
+  )
+}
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Home from '../pages/Home/Home';
+import MainContainer from '../pages/mainContainer/MainContainer';
+function App() {
+
+  return (
+    <>
+      <Home/>
+      <MainContainer/>
+    </>
+  )
+}
+
+export default App
